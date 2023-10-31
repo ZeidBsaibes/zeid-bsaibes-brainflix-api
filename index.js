f1cf3a1bdc87bb74643331c9b7637ef67428d606
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-10-31 23:55:40 +0000

    add post functionality, add validate, add write to JSON

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const crypto = require("crypto");
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
@@ -10,10 +11,23 @@ app.listen(`${PORT}`, () => {
   console.log(`from index.js, listening on port ${PORT}`);
 });
 
+app.use(express.json());
+
 app.get("/videos", (req, res) => {
   try {
-    const videosArray = fs.readFileSync("./data/videos.json");
-    res.status(200).send(videosArray);
+    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
+    const videosArraySelectedKeys = videosArray.map(
+      ({ id, title, channel, image }) => {
+        return {
+          id,
+          title,
+          channel,
+          image,
+        };
+      }
+    );
+
+    res.status(200).send(videosArraySelectedKeys);
   } catch (error) {
     res.status(500).send(`something went wrong, have a look at this ${error}`);
     console.error(error);
@@ -46,6 +60,70 @@ app.get("/videos/:id", (req, res) => {
 
 app.post("/videos", (req, res) => {
   try {
+    const {
+      title,
+      channel,
+      image,
+      description,
+      views,
+      likes,
+      duration,
+      video,
+    } = req.body;
+
+    const errors = [];
+
+    if (!title) {
+      errors.push({ message: "missing title " });
+    }
+    if (!channel) {
+      errors.push({ message: "missing channel" });
+    }
+    if (!image) {
+      errors.push({ message: "missing image URL" });
+    }
+    if (!description) {
+      errors.push({ message: "missing video description" });
+    }
+    if (!views) {
+      errors.push({ message: "no views number" });
+    }
+    if (!likes) {
+      errors.push({ message: "no likes number" });
+    }
+    if (!duration) {
+      errors.push({ message: "no duration information" });
+    }
+    if (!video) {
+      errors.push({ message: "no video URL" });
+    }
+    if (errors.length > 0) {
+      return res.status(400).send(errors);
+    }
+
+    const newVideo = {
+      id: crypto.randomUUID(),
+      title: title,
+      channel: channel,
+      image: image,
+      description: description,
+      views: views,
+      likes: likes,
+      duration: duration,
+      video: video,
+      timestamp: Date.now(),
+      comments: [],
+    };
+    // console.log(`this is the new video`, newVideo);
+
+    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
+
+    videosArray.push(newVideo);
+    console.log(videosArray);
+
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videosArray));
+
+    res.status(201).send(newVideo);
   } catch (error) {
     console.error(error);
   }
