67d34472977c5f669a0e703e16b4003b6fcac0ec
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-05 21:08:52 +0000

    Add static image for upload

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const PORT = process.env.PORT;
 const fs = require("fs");
 const cors = require("cors");
 
+const videos = require("./routes/videos");
 app.use(express.json());
+
+app.use("/videos", videos);
 
 // need to add all  images and server from the backend
 app.use(cors());
@@ -18,120 +21,29 @@ app.listen(`${PORT}`, () => {
   console.log(`from index.js, listening on port ${PORT}`);
 });
 
-app.get("/videos", (req, res) => {
-  try {
-    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
-    const videosArraySelectedKeys = videosArray.map(
-      ({ id, title, channel, image }, index) => {
-        return {
-          id,
-          title,
-          channel,
-          image: `http://localhost:${PORT}/public/images/image${index}.jpeg`,
-        };
-      }
-    );
-    console.log(`/videos get requested`);
-    res.status(200).send(videosArraySelectedKeys);
-  } catch (error) {
-    res.status(500).send(`something went wrong, have a look at this ${error}`);
-    console.error(error);
-  }
-});
+// app.get("/videos/:id", (req, res) => {
+//   try {
+//     const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
+//     const requestedVideoId = req.params.id;
+//     console.log(`/ videos/id get request`);
+//     const foundVideo = videosArray.find((video) => {
+//       return video.id === requestedVideoId;
+//     });
+//     if (!foundVideo) {
+//       return res.status(404).send({
+//         error: {
+//           code: "RESOURCE_NOT_FOUND",
+//           message: "No video with that id exists",
+//         },
+//       });
+//     } else if (foundVideo) {
+//       return res.status(200).send(foundVideo);
+//     }
 
-app.get("/videos/:id", (req, res) => {
-  try {
-    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
-    const requestedVideoId = req.params.id;
-    console.log(`/ videos/id get request`);
-    const foundVideo = videosArray.find((video) => {
-      return video.id === requestedVideoId;
-    });
-    if (!foundVideo) {
-      return res.status(404).send({
-        error: {
-          code: "RESOURCE_NOT_FOUND",
-          message: "No video with that id exists",
-        },
-      });
-    } else if (foundVideo) {
-      return res.status(200).send(foundVideo);
-    }
-
-    res.send(foundVideo);
-  } catch (error) {
-    res.status(500).send(`something went wrong, have a look at this ${error}`);
-  }
-});
-
-app.post("/videos", (req, res) => {
-  try {
-    const {
-      title,
-      channel,
-      image,
-      description,
-      views,
-      likes,
-      duration,
-      video,
-    } = req.body;
-
-    const errors = [];
-
-    if (!title) {
-      errors.push({ message: "missing title " });
-    }
-    if (!channel) {
-      errors.push({ message: "missing channel" });
-    }
-    if (!image) {
-      errors.push({ message: "missing image URL" });
-    }
-    if (!description) {
-      errors.push({ message: "missing video description" });
-    }
-    if (!views) {
-      errors.push({ message: "no views number" });
-    }
-    if (!likes) {
-      errors.push({ message: "no likes number" });
-    }
-    if (!duration) {
-      errors.push({ message: "no duration information" });
-    }
-    if (!video) {
-      errors.push({ message: "no video URL" });
-    }
-    if (errors.length > 0) {
-      return res.status(400).send(errors);
-    }
-
-    const newVideo = {
-      id: crypto.randomUUID(),
-      title: title,
-      channel: channel,
-      image: image,
-      description: description,
-      views: views,
-      likes: likes,
-      duration: duration,
-      video: video,
-      timestamp: Date.now(),
-      comments: [],
-    };
-    console.log(`this is the new video`, newVideo);
-
-    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
-
-    videosArray.push(newVideo);
-
-    fs.writeFileSync("./data/videos.json", JSON.stringify(videosArray));
-
-    res.status(201).send(newVideo);
-  } catch (error) {
-    console.error(error);
-  }
-});
+//     res.send(foundVideo);
+//   } catch (error) {
+//     res.status(500).send(`something went wrong, have a look at this ${error}`);
+//   }
+// });
 
 // serve static images
---
+++ routes/videos.js
@@ -0,0 +1,133 @@
+const express = require("express");
+const crypto = require("crypto");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
+const fs = require("fs");
+const cors = require("cors");
+const router = express.Router();
+
+router.use(express.json());
+router.use(cors());
+router.use("/public", express.static("public"));
+
+router.get("/", (req, res) => {
+  try {
+    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
+    const videosArraySelectedKeys = videosArray.map(
+      ({ id, title, channel, image }, index) => {
+        return {
+          id,
+          title,
+          channel,
+          image:
+            index < 8
+              ? `http://localhost:${PORT}/public/images/image${index}.jpeg`
+              : `http://localhost:${PORT}/public/images/mountains.jpg`,
+        };
+      }
+    );
+    console.log(`/videos get requested`);
+    res.status(200).send(videosArraySelectedKeys);
+  } catch (error) {
+    res.status(500).send(`something went wrong, have a look at this ${error}`);
+    console.error(error);
+  }
+});
+
+router.post("/", (req, res) => {
+  try {
+    const {
+      title,
+      channel,
+      image,
+      description,
+      views,
+      likes,
+      duration,
+      video,
+    } = req.body;
+
+    const errors = [];
+
+    if (!title) {
+      errors.push({ message: "missing title " });
+    }
+    if (!channel) {
+      errors.push({ message: "missing channel" });
+    }
+    if (!image) {
+      errors.push({ message: "missing image URL" });
+    }
+    if (!description) {
+      errors.push({ message: "missing video description" });
+    }
+    if (!views) {
+      errors.push({ message: "no views number" });
+    }
+    if (!likes) {
+      errors.push({ message: "no likes number" });
+    }
+    if (!duration) {
+      errors.push({ message: "no duration information" });
+    }
+    if (!video) {
+      errors.push({ message: "no video URL" });
+    }
+    if (errors.length > 0) {
+      return res.status(400).send(errors);
+    }
+
+    const newVideo = {
+      id: crypto.randomUUID(),
+      title: title,
+      channel: channel,
+      image: image,
+      description: description,
+      views: views,
+      likes: likes,
+      duration: duration,
+      video: video,
+      timestamp: Date.now(),
+      comments: [],
+    };
+    console.log(`this is the new video`, newVideo);
+
+    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
+
+    videosArray.push(newVideo);
+
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videosArray));
+
+    res.status(201).send(newVideo);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+router.get("/:id", (req, res) => {
+  try {
+    const videosArray = JSON.parse(fs.readFileSync("./data/videos.json"));
+    const requestedVideoId = req.params.id;
+    console.log(`/ videos/id get request`);
+    const foundVideo = videosArray.find((video) => {
+      return video.id === requestedVideoId;
+    });
+    if (!foundVideo) {
+      return res.status(404).send({
+        error: {
+          code: "RESOURCE_NOT_FOUND",
+          message: "No video with that id exists",
+        },
+      });
+    } else if (foundVideo) {
+      return res.status(200).send(foundVideo);
+    }
+
+    res.send(foundVideo);
+  } catch (error) {
+    res.status(500).send(`something went wrong, have a look at this ${error}`);
+  }
+});
+
+module.exports = router;
